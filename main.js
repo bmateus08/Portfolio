f75c2d7a026f498ca82778635aadd2fd9aeb0480
Mateus <mateus-bcp@hotmail.com>
2023-09-19 15:17:40 -0300

    fisrt commit

---
+++ main.js
@@ -0,0 +1,3 @@
+ <script>
+          alert("Para visualizar melhor o site e obter um melhor feedback sobre os elementos por favor entrar em um disitivo com a tela grande, de preferência noteeboks ou computadores, obrigado =)")
+      </script>
